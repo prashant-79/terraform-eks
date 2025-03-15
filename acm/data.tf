5a1e7cbd2030e0a6d355ecbc5aa47bc6776ab695
Nidhi Gupta <nidhigupta@Nidhis-MacBook-Pro.local>
2025-03-15 17:48:15 -0400

    test

--- acm/data.tf
+++ acm/data.tf
@@ -2,3 +2,4 @@
 data "aws_route53_zone" "example" {
   name = "devops4solutions.com"  # Replace with your domain
 }
+
--- monitoring/ingress.tf
+++ monitoring/ingress.tf
@@ -82,3 +82,4 @@ resource "kubectl_manifest" "grafana_ingress" {
     }
   })
 }
+
